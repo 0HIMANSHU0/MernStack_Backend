606906ce050454861844da4c122b274656163a1b
Himanshu Saini <himanshusaini0612@gmail.com>
2023-08-19 13:14:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+require("./db/conn");
+const app = express();
+const cors = require("cors");
+const router = require("./Routes/router");
+const PORT = 6010;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use("/uploads",express.static("./uploads"));
+app.use("/files",express.static("./public/files"));
+
+app.listen(PORT, (req, res)=>{
+ console.log(`Server Started at Port NO: ${PORT}`)
+})
